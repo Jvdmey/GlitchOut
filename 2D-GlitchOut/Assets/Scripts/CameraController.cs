23a01a48b9dedba6d25e15177b3333ae54509327
Justin <Justinvdmey@hotmail.com>
2020-09-03 10:59:26 +0200

    Player&Camera Movement

---
+++ 2D-GlitchOut/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region Variables
+    public Transform player;
+
+    [Space]
+    [Header("Input Settings:")]
+    public float smoothing;
+    public Vector2 minBoundary;
+    public Vector2 maxBoundary;
+#endregion
+#region Methods
+    void Update()
+    {
+        Vector3 playerPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+        playerPos.x = Mathf.Clamp(player.position.x, minBoundary.x, maxBoundary.x);
+        playerPos.y = Mathf.Clamp(player.position.y, minBoundary.y, maxBoundary.y); 
+
+        transform.position = Vector3.Lerp(transform.position, playerPos, smoothing);
+    }
+    #endregion
+}
---
+++ 2D-GlitchOut/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+#region Varaibles
+[Header("Input Settings:")]
+public float movementSpeed;
+public float energy;
+public float decreaseSpeed;
+
+[Space]
+[Header("Character statistics:")]
+public Vector2 movementDirection;
+
+[Space]
+[Header("References:")]
+private Rigidbody2D rb;
+
+#endregion
+
+#region Methods
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        movementDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        bool isMovingHorizontal = Mathf.Abs(movementDirection.x) > 0.5f;
+        bool isMovingvertical = Mathf.Abs(movementDirection.y) > 0.5f;
+        bool wasMovingVertical = false;
+
+        Vector2 lastMove;
+        
+
+        if(isMovingHorizontal && isMovingvertical)
+        {
+            if(wasMovingVertical)
+            {
+                rb.velocity = new Vector2(movementDirection.x * movementSpeed * Time.deltaTime, 0);
+                lastMove = new Vector2( movementDirection.x, 0f);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, movementDirection.y * movementSpeed * Time.deltaTime);
+                lastMove = new Vector2(0f, movementDirection.y);
+            }
+        }
+        else if(isMovingHorizontal)
+        {
+            rb.velocity = new Vector2(movementDirection.x * movementSpeed * Time.deltaTime, 0);
+            wasMovingVertical = false;
+            lastMove = new Vector2( movementDirection.x, 0f);
+        }
+        else if(isMovingvertical)
+        {
+            rb.velocity = new Vector2(0, movementDirection.y * movementSpeed * Time.deltaTime);
+            wasMovingVertical = true;
+            lastMove = new Vector2(0f, movementDirection.y);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift) && energy > 0)
+        {
+            energy -= decreaseSpeed * Time.deltaTime;
+
+            energy = Mathf.Max(0, energy);
+
+            movementSpeed = 150;
+        }
+        else
+        {
+            movementSpeed = 90;
+        }
+    }
+#endregion
+}
