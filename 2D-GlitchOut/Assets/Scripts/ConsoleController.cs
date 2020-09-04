1e6561fd157c52a8637b57206c03acd1d1e81dec
MCM013 <mick.hofmeijer@gmail.com>
2020-09-04 10:23:07 +0200

    Please dont break please dont break

---
+++ 2D-GlitchOut/Assets/Scripts/ConsoleController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+public class ConsoleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
