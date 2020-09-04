2b340f84da09082ef0ab0f41ff5cb675a276ac67
MCM013 <mick.hofmeijer@gmail.com>
2020-09-04 12:28:13 +0200

    TEST DONT WORK PLS

--- 2D-GlitchOut/Assets/Scripts/TextAdventureTEST.cs
+++ 2D-GlitchOut/Assets/Scripts/TextAdventureTEST.cs
@@ -11,37 +11,39 @@ public class TextAdventureTEST : MonoBehaviour
     public string myText;
 
 
-	// Checks if there is anything entered into the input field.
 
-
-	public void Start()
-	{
-		//Adds a listener that invokes the "LockInput" method when the player finishes editing the main input field.
-		//Passes the main input field into the method when "LockInput" is invoked
-		mainInputField.onEndEdit.AddListener(delegate { LockInput(mainInputField); });
-	}
-
-	void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown (KeyCode.Return))
-        {
-            inputText.text = "HELP";
+        
+    }
+    void Update()
+    {
+        
+        BeginCheck();
+
+        //if (Input.GetKeyDown (KeyCode.Return))
+        //{
+        //    inputText.text = "HELP";
             
+
+        //    Debug.Log("FEEDBACK");
+        //}
+    }
+    public void BeginCheck()
+    {
+        string answer = "Hello";
+        myText = mainInputField.text;
+        myText = inputText.text;
+
+        if (answer.ToString() == mainInputField.text)
+        {
+            mainInputField.text = "HELP";
+
 
             Debug.Log("FEEDBACK");
         }
-        
     }
-	public void LockInput(InputField input)
-	{
-		if (input.text.Length > 0)
-		{
-			Debug.Log("Text has been entered");
-		}
-		else if (input.text.Length == 0)
-		{
-			Debug.Log("Main Input Empty");
-		}
+
+
 	}
 
-}
