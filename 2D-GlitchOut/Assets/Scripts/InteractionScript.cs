08a26d2fbe2c4f6521bfebbbd89a67311382c667
EmperorFuckface <thijsapeldoorn@gmail.com>
2020-09-07 13:18:47 +0200

    interactables added

---
+++ 2D-GlitchOut/Assets/Scripts/InteractionScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionScript : MonoBehaviour
+{
+    bool colliderCheck;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            colliderCheck = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            colliderCheck = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (colliderCheck == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
