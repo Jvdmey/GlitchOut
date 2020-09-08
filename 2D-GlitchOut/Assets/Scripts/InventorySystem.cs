0623b5b7e03f6643b788e5a467b671fb476bac25
EmperorFuckface <thijsapeldoorn@gmail.com>
2020-09-08 12:03:58 +0200

    moved some of ties' files to the right folders

---
+++ 2D-GlitchOut/Assets/Scripts/InventorySystem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    //add this script to the player
+    public bool[] isFull;
+    public GameObject[] slots;
+}
---
+++ 2D-GlitchOut/Assets/Scripts/PickUpAbility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpAbility : MonoBehaviour
+{
+    // add this script to your pick up 
+    private InventorySystem inventory;
+    //add the pickup prefab to the ItemButton
+    public GameObject ItemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<InventorySystem>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for(int i = 0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false)
+                {
+                    //item word geadd naar de inventory
+                    inventory.isFull[i] = true;
+                    Instantiate(ItemButton, inventory.slots[i].transform);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
