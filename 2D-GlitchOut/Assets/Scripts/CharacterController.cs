0cdd57288f6b95cd17e54debe22212a8315565d7
EmperorFuckface <thijsapeldoorn@gmail.com>
2020-09-18 10:57:25 +0200

    added ai whom follows player

--- 2D-GlitchOut/Assets/Scripts/CharacterController.cs
+++ 2D-GlitchOut/Assets/Scripts/CharacterController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    #region Varaibles
+#region Variables
 
     public Animator animator;
 
---
+++ 2D-GlitchOut/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float speed;
+    public float stoppingDistance;
+
+    private Transform target;
+
+    bool playerCheck;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerCheck = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerCheck = false;
+        }
+    }
+
+    void Update()
+    {
+        if (playerCheck == true)
+        {
+            if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+        }
+    }
+}
