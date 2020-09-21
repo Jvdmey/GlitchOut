a68a1c2169cd5b62e477f5f1f2ac2f48bc7437d3
Asuu-rahh <52154064+Asuu-rahh@users.noreply.github.com>
2020-09-21 14:34:27 +0200

    shooting (not done)(plssendhelp)

---
+++ 2D-GlitchOut/Assets/Scripts/ArmWeapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmWeapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject lazerBulletPrefab;
+    CharacterController refCharacterController;
+
+    private void Start()
+    {
+        refCharacterController = gameObject.GetComponent<CharacterController>();
+    
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        if(refCharacterController.movementDirection.x > 0)
+        {
+            //firepoint moves here
+        }
+        void Shoot()
+        {
+            Instantiate(lazerBulletPrefab, firePoint.position, firePoint.rotation);
+        }
+    }
+}
